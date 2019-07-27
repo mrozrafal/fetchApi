3c28d1117e5d7a8170510aaf81914de35d396666
Rafal Mroz <rafalmroz30@gmail.com>
2019-07-27 13:06:37 +0200

    feat: list of github repos

--- src/js/main.js
+++ src/js/main.js
@@ -15,8 +15,19 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
+const list = document.querySelector('.list--js');
 
 
-console.log(`Hello world!`);
 
+fetch('https://api.github.com/users/mrozrafal/repos?sort=updated&direction=desc')
+.then(resp => resp.json())
+.then(resp => {
+  const repos = resp;
+for (const repo of repos){
+list.innerHTML+=`<li><a href ="${repo.html_url}">${repo.name}</a> </li>`;
+}
+})
 
+.catch(err =>{
+  console.log(err);
+})
